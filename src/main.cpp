268941b4239385ef21df6e4a257994546100ad64
mosilaana1 <mosila.ana1@gmail.com>
2026-05-08 00:09:47 -0700

    prima salvare

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "ThingSpeak.h"
+#include "DHT.h"
+
+const char* SECRE_SSID = "=)";
+const char* SECRET_PASS = "12345678";
+unsigned long ChannelID =  3374169;
+const char* myWriteAPIKey = "ENCQU6T9ZX7OZGQS";
+
+void setup() {
+}
+
+void loop() {
+}
